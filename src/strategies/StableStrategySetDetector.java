3ce46718e3e2217c346cfff9272ade39c0cbe4ef
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-12-01 00:37:30 +0100

    Stub of strategies/StableStrategySetDetector.java

---
+++ src/strategies/StableStrategySetDetector.java
@@ -0,0 +1,12 @@
+package strategies;
+
+import java.util.LinkedHashSet;
+
+public class StableStrategySetDetector {
+	
+	public static Boolean analyze(LinkedHashSet<Strategy> setOfStrategies){
+		boolean result = false;
+		return result;
+	}
+
+}
