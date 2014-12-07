92d4e8afb6617386c0318759a0a8202708a03bc3
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-12-07 20:32:47 +0100

    Proper kind of singleton

--- src/utils/LaunchStatistics.java
+++ src/utils/LaunchStatistics.java
@@ -2,22 +2,33 @@ package utils;
 
 import constants.Constraints;
 
+/***
+ * Used for outputting final results!
+ * 
+ * @author Oskar Jarczyk
+ * @since 1.0
+ */
 public class LaunchStatistics {
-	
+
 	public static LaunchStatistics singleton = null;
-	
+
 	public int agentCount = 0;
 	public int taskCount = 0;
 	public boolean expDecay = false;
 	public boolean fullyLearnedAgentsLeave = false;
-	
+
 	public boolean granularity = false;
 	public String granularityType = Constraints.EMPTY_STRING;
-	
+
 	public boolean experienceCutPoint = false;
-	
-	public LaunchStatistics(){
-		singleton = this;
+
+	public LaunchStatistics() {
+		if (singleton != null) {
+			throw new UnsupportedOperationException(
+					"There is already an instance of LaunchStatistics.");
+		} else {
+			singleton = this;
+		}
 	}
 
 }
