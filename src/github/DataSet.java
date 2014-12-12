a945ee55587aff842257f9d8f2e214d8ea972d67
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-12-12 02:23:08 +0100

    DataSet updated

--- src/github/DataSet.java
+++ src/github/DataSet.java
@@ -3,7 +3,7 @@ package github;
 public class DataSet {
 	
 	private enum Type {
-		SocketListener, FileSoup, Mockup, AgentTestUniverse;
+		SocketListener, Sqlite, FileSoup, Mockup, AgentTestUniverse;
 	}
 	
 	public int Current;
@@ -15,12 +15,22 @@ public class DataSet {
 	public void setCurrent(String name){
 		if (name.toLowerCase().equals("socketlistener")){
 			Current = Type.SocketListener.ordinal();
+		} else if (name.toLowerCase().equals("sqlite")){
+			Current = Type.Sqlite.ordinal();
 		} else if (name.toLowerCase().equals("filesoup")){
 			Current = Type.FileSoup.ordinal();
 		} else if (name.toLowerCase().equals("mockup")){
 			Current = Type.Mockup.ordinal();
 		} else if (name.toLowerCase().equals("agenttestuniverse")){
 			Current = Type.AgentTestUniverse.ordinal();
+		}
+	}
+	
+	public Boolean isContinuus(){
+		if (Current == Type.Sqlite.ordinal()){
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
