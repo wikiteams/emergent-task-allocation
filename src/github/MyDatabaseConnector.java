2231f1cdc2b904b5931f8bc0cf55a54d47ab939a
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-12-12 14:00:32 +0100

    working on MyDatabaseConnector.java

--- src/github/MyDatabaseConnector.java
+++ src/github/MyDatabaseConnector.java
@@ -2,25 +2,40 @@ package github;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.commons.lang3.SystemUtils;
 
 public class MyDatabaseConnector {
+	
+	private static Connection connection = null;
+	private static ResultSet rs = null;
 
 	public static Boolean init() {
 		boolean success = true;
-		Connection c = null;
+		String filename = "test.db";
 		try {
 			Class.forName("org.sqlite.JDBC");
-			String connectionString = SystemUtils.IS_OS_LINUX ? "jdbc:sqlite:test.db"
-					: "jdbc:sqlite:test.db";
-			c = DriverManager.getConnection(connectionString);
+			String connectionString = SystemUtils.IS_OS_LINUX ? "jdbc:sqlite:" + filename
+					: "jdbc:sqlite:" + filename;
+			connection = DriverManager.getConnection(connectionString);
+			createResultSet();
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			success = false;
+			return false;
 		}
 		System.out.println("Opened database successfully");
 		return success;
+	}
+	
+	private static Boolean createResultSet() throws SQLException{
+		Boolean result = true;
+		Statement statement = connection.createStatement();
+	    statement.setQueryTimeout(360);  // set timeout to 360 sec.
+	    rs = statement.executeQuery("select * from person");
+	    return result;
 	}
 
 }
