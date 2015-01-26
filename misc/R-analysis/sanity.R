7b6c71ec1581c5cdc6edb92ffd0296cec9354888
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2015-01-27 00:42:42 +0100

    More of code :)

--- misc/R-analysis/sanity.R
+++ misc/R-analysis/sanity.R
@@ -2,6 +2,21 @@ install.packages("Rcpp")
 install.packages("dplyr")
 library(dplyr)
 
+setwd("~/simphony_model_1422230575260/instance_1")
+dane_instance_1 <- read.csv('agents_nonaggr_state.2015.Jan.26.01_06_26.csv', sep=";", quote="\"", header = TRUE, dec=".")
+dane_instance_1 <- head(dane_instance_1, -1)
+setwd("~/simphony_model_1422230575260/instance_2")
+dane_instance_2 <- read.csv('agents_nonaggr_state.2015.Jan.26.01_06_26.csv', sep=";", quote="\"", header = TRUE, dec=".")
+dane_instance_2 <- head(dane_instance_2, -1)
+
 # dane_pogrup <- group_by(dane, EvolutionGeneration, Id, TaskStrategy)
-dane_pogrup <- group_by(dane[dane$EvolutionIteration == 1,], EvolutionGeneration, Id, TaskStrategy)
+dane_pogrup <- group_by(dane_instance_1[dane_instance_1$EvolutionIteration == 1,], EvolutionGeneration, Id, TaskStrategy)
 summarize (  group_by(dane_pogrup, EvolutionGeneration, TaskStrategy), StratCount = n() )
+wyniki <- summarize (  group_by(dane_pogrup, EvolutionGeneration, TaskStrategy), StratCount = n() )
+
+p <- ggplot() + 
+  geom_line(data = wyniki[wyniki$TaskStrategy == "HETEROPHYLY_EXP", ], aes(x = EvolutionGeneration, y = StratCount, color = "heterophyly")) +
+  geom_line(data = wyniki[wyniki$TaskStrategy == "HOMOPHYLY_EXP", ], aes(x = EvolutionGeneration, y = StratCount, color = "homophyly"))  +
+  geom_line(data = wyniki[wyniki$TaskStrategy == "PREFERENTIAL", ], aes(x = EvolutionGeneration, y = StratCount, color = "preferential"))  +
+  xlab('EvolutionGeneration') +
+  ylab('StratCount') + labs(color="Strategies")
