0a8019584b4f707de019cde23cd5a21a23f39202
Oskar Jarczyk <oskar.jarczyk@pjwstk.edu.pl>
2015-02-02 16:11:44 +0100

    Create analysis.R
    stub of R code file

---
+++ misc/R-analysis/analysis.R
@@ -0,0 +1,14 @@
+setwd("~/symulator/simphony_model_1422759723448/")
+
+library(dplyr)
+
+instance_1 <- read.csv('instance_1/agents_nonaggr_state.2015.lut.01.04_04_09.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_2 <- read.csv('instance_2/agents_nonaggr_state.2015.lut.01.04_04_11.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_3 <- read.csv('instance_3/agents_nonaggr_state.2015.lut.01.04_04_10.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_4 <- read.csv('instance_4/agents_nonaggr_state.2015.lut.01.04_04_10.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_5 <- read.csv('instance_5/agents_nonaggr_state.2015.lut.01.04_04_10.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_6 <- read.csv('instance_6/agents_nonaggr_state.2015.lut.01.04_04_10.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_7 <- read.csv('instance_7/agents_nonaggr_state.2015.lut.01.04_04_11.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_8 <- read.csv('instance_8/agents_nonaggr_state.2015.lut.01.04_04_11.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_9 <- read.csv('instance_9/agents_nonaggr_state.2015.lut.01.04_04_10.csv', sep=";", quote="\"", header = TRUE, dec=".")
+instance_10 <- read.csv('instance_10/agents_nonaggr_state.2015.lut.01.04_04_10.csv', sep=";", quote="\"", header = TRUE, dec=".")
