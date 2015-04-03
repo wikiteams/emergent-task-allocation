ee2a2d605f7ff49882e4d06c1616c497774227ab
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2015-04-04 00:17:01 +0200

    In UtilityFactory.java aside of method which accepts a collection, I made a second method for single-choice

--- src/utils/UtilityFactory.java
+++ src/utils/UtilityFactory.java
@@ -4,10 +4,21 @@ import intelligence.UtilityTypes;
 import collaboration.SimulationParameters;
 import collaboration.Utility.UtilityType;
 
+/**
+ * Setting the utility function of the evolutionary mechanism
+ * 
+ * @author Oskar Jarczyk
+ * @since 2.0.9
+ * @version 2.0.10
+ */
 public class UtilityFactory {
 
 	public static void randomizeUtility(UtilityType[] utilityTypes) {
 		UtilityType chosen = ObjectsHelper.randomFrom(utilityTypes);
+		setUtility(chosen);
+	}
+	
+	public static void setUtility(UtilityType chosen){
 		if (chosen.equals(UtilityTypes.LearningSkills)) {
 			SimulationParameters.isAgentOrientedUtility = true;
 			SimulationParameters.isTaskOrientedUtility = false;
