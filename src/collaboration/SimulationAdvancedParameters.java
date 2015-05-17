f3e2aa38a5aa282fab6d60b25b281f167df36d3b
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2015-05-17 23:51:52 +0200

    Fixed agent modelling, specially the problem of experience's top-value, changes in AgentModeling.java and SimulationAdvancedParameters.java

--- src/collaboration/SimulationAdvancedParameters.java
+++ src/collaboration/SimulationAdvancedParameters.java
@@ -6,5 +6,6 @@ public class SimulationAdvancedParameters {
 	public static boolean enableNetwork = false;
 	public static final int IMPACT_MEMORY = 3;
 	public static final int agentSkillsMaximumExperience = 1000;
+	public static final int lowestTop = 10000;
 
 }
